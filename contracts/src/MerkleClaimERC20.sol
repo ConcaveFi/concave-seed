95ea3bb1abb19963e99f9cafc6a71825dd174320
0xBarista <0xBarista@protonmail.com>
2022-01-09 11:52:50 -0500

    added comments

--- contracts/src/MerkleClaimERC20.sol
+++ contracts/src/MerkleClaimERC20.sol
@@ -15,8 +15,11 @@ contract MerkleClaimERC20 is ERC20 {
 
   /// ============ Immutable storage ============
 
+  /// @notice Mapping of addresses of valid deposit tokens
   mapping(address => bool) public approvedDeposits;
+  /// @notice ratio of deposits to claimed tokens (i.e: to claim 1 token, must deposit 3*1 of approvedDeposits)
   uint256 public constant ratio = 3;
+  /// @notice treasury address to which deposited tokens are sent
   address public immutable treasury;
 
   /// @notice ERC20-claimee inclusion root
@@ -33,7 +36,7 @@ contract MerkleClaimERC20 is ERC20 {
   error AlreadyClaimed();
   /// @notice Thrown if address/amount are not part of Merkle tree
   error NotInMerkle();
-
+  /// @notice Thrown in deposit token is not approved
   error NotValidDepositToken();
 
   /// ============ Constructor ============
@@ -43,6 +46,9 @@ contract MerkleClaimERC20 is ERC20 {
   /// @param _symbol of token
   /// @param _decimals of token
   /// @param _merkleRoot of claimees
+  /// @param _FRAX address of FRAX to set as approvedDeposit
+  /// @param _DAI address of DAI to set as approvedDeposit
+  /// @param _treasury address
   constructor(
     string memory _name,
     string memory _symbol,
@@ -53,9 +59,9 @@ contract MerkleClaimERC20 is ERC20 {
     address _treasury
   ) ERC20(_name, _symbol, _decimals) {
     merkleRoot = _merkleRoot; // Update root
-    approvedDeposits[_FRAX] = true;
-    approvedDeposits[_DAI] = true;
-    treasury = _treasury;
+    approvedDeposits[_FRAX] = true; // set FRAX as an approvedDeposit
+    approvedDeposits[_DAI] = true; // set DAI as an approvedDeposit
+    treasury = _treasury; // set treasury address
   }
 
   /// ============ Events ============
@@ -70,11 +76,12 @@ contract MerkleClaimERC20 is ERC20 {
   /// @notice Allows claiming tokens if address is part of merkle tree
   /// @param to address of claimee
   /// @param amount of tokens owed to claimee
+  /// @param token address of token user wishes to deposit
   /// @param proof merkle proof to prove address and amount are in tree
   function claim(address to, uint256 amount, address token, bytes32[] calldata proof) external {
     // Throw if address has already claimed tokens
     if (hasClaimed[to]) revert AlreadyClaimed();
-
+    // Throw if deposit token isn't approved (i.e isn't FRAX or DAI)
     if (!approvedDeposits[token]) revert NotValidDepositToken();
 
     // Verify merkle proof, or revert if not in tree
@@ -87,7 +94,7 @@ contract MerkleClaimERC20 is ERC20 {
 
     // Mint tokens to address
     _mint(to, amount);
-
+    // Transfer amount*ratio of token to treasury address
     IERC20(token).transferFrom(msg.sender,treasury,amount*ratio);
     // Emit claim event
     emit Claim(to, amount);
