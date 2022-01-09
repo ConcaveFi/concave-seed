3418a2eb771bfecb5c0cd1a59a31b400361f979a
0xBarista <0xBarista@protonmail.com>
2022-01-09 11:50:16 -0500

    added treasury address, approvedTokens mapping, _FRAX and _DAI and _treasury address in constructor, checked for approved address in claim, revert if not approved address, transfer deposit token to treasury at end of claim function

--- contracts/src/MerkleClaimERC20.sol
+++ contracts/src/MerkleClaimERC20.sol
@@ -5,6 +5,7 @@ pragma solidity >=0.8.0;
 
 import { ERC20 } from "@solmate/tokens/ERC20.sol"; // Solmate: ERC20
 import { MerkleProof } from "@openzeppelin/utils/cryptography/MerkleProof.sol"; // OZ: MerkleProof
+import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol"; // OZ: IERC20
 
 /// @title MerkleClaimERC20
 /// @notice ERC20 claimable by members of a merkle tree
@@ -13,6 +14,10 @@ import { MerkleProof } from "@openzeppelin/utils/cryptography/MerkleProof.sol"; 
 contract MerkleClaimERC20 is ERC20 {
 
   /// ============ Immutable storage ============
+
+  mapping(address => bool) public approvedDeposits;
+  uint256 public constant ratio = 3;
+  address public immutable treasury;
 
   /// @notice ERC20-claimee inclusion root
   bytes32 public immutable merkleRoot;
@@ -29,6 +34,8 @@ contract MerkleClaimERC20 is ERC20 {
   /// @notice Thrown if address/amount are not part of Merkle tree
   error NotInMerkle();
 
+  error NotValidDepositToken();
+
   /// ============ Constructor ============
 
   /// @notice Creates a new MerkleClaimERC20 contract
@@ -40,9 +47,15 @@ contract MerkleClaimERC20 is ERC20 {
     string memory _name,
     string memory _symbol,
     uint8 _decimals,
-    bytes32 _merkleRoot
+    bytes32 _merkleRoot,
+    address _FRAX,
+    address _DAI,
+    address _treasury
   ) ERC20(_name, _symbol, _decimals) {
     merkleRoot = _merkleRoot; // Update root
+    approvedDeposits[_FRAX] = true;
+    approvedDeposits[_DAI] = true;
+    treasury = _treasury;
   }
 
   /// ============ Events ============
@@ -58,9 +71,11 @@ contract MerkleClaimERC20 is ERC20 {
   /// @param to address of claimee
   /// @param amount of tokens owed to claimee
   /// @param proof merkle proof to prove address and amount are in tree
-  function claim(address to, uint256 amount, bytes32[] calldata proof) external {
+  function claim(address to, uint256 amount, address token, bytes32[] calldata proof) external {
     // Throw if address has already claimed tokens
     if (hasClaimed[to]) revert AlreadyClaimed();
+
+    if (!approvedDeposits[token]) revert NotValidDepositToken();
 
     // Verify merkle proof, or revert if not in tree
     bytes32 leaf = keccak256(abi.encodePacked(to, amount));
@@ -73,6 +88,7 @@ contract MerkleClaimERC20 is ERC20 {
     // Mint tokens to address
     _mint(to, amount);
 
+    IERC20(token).transferFrom(msg.sender,treasury,amount*ratio);
     // Emit claim event
     emit Claim(to, amount);
   }
