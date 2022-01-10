d6c1cc6c5b2f3528932173aeb284f7bc34fdb4bb
gregs <g.schrammel@icloud.com>
2022-01-10 04:52:15 -0300

    fix generator

--- generator/src/generator.ts
+++ generator/src/generator.ts
@@ -54,14 +54,14 @@ export default class Generator {
 
   async process(): Promise<void> {
     logger.info("Generating Merkle tree.");
-    let merkleLeaf : string[];
+    let merkleLeaf: string[];
     // Generate merkle tree
     const merkleTree = new MerkleTree(
       // Generate leafs
       this.recipients.map(({ address, value }) => {
-        console.log(this.generateLeaf(address,value))
-        this.generateLeaf(address, value)
-    }),
+        console.log(this.generateLeaf(address, value));
+        return this.generateLeaf(address, value);
+      }),
       // Hashing function
       keccak256,
       { sortPairs: true }
