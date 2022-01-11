402bc37af38e7d7402f3fe96e193980fbd5338eb
gregs <g.schrammel@icloud.com>
2022-01-10 21:35:28 -0300

    update

--- frontend/lib/claim.ts
+++ frontend/lib/claim.ts
@@ -1,32 +1,11 @@
 import { ethers, Signer } from 'ethers'
 import { signERC2612Permit, signDaiPermit } from 'eth-permit'
 import { merkleTree, getClaimableAmount, leafOf } from './merkletree'
-import { getRopstenSdk } from '@dethcrypto/eth-sdk-client'
+import { getRopstenSdk, RopstenSdk } from '@dethcrypto/eth-sdk-client'
 import { Provider } from '@ethersproject/abstract-provider'
-import contractsConfig from 'eth-sdk.config'
-
-const daiAddresses = Object.values(contractsConfig.contracts)
-  .map((network) => network?.dai as string)
-  .filter(Boolean)
+import { Dai } from '.dethcrypto/eth-sdk-client/esm/types'
 
 export const inputTokens = ['dai', 'frax']
-
-const signPermit = (
-  provider: Provider,
-  token: string,
-  owner: string,
-  spender: string,
-  deadline?: number,
-  value?: string,
-  nonce?: number,
-) => {
-  if (daiAddresses.includes(token))
-    return signDaiPermit(provider, token, owner, spender, deadline, nonce).then((a) => ({
-      ...a,
-      deadline: a.expiry,
-    }))
-  return signERC2612Permit(provider, token, owner, spender, value, deadline, nonce)
-}
 
 export const claim = async (
   address: string,
@@ -38,25 +17,41 @@ export const claim = async (
   if (!address) throw new Error('Not Authenticated')
 
   const formattedToAddress = ethers.utils.getAddress(address)
-  const maxAmount = getClaimableAmount(address)
-  const proof: string[] = merkleTree.getHexProof(leafOf(address))
+  const maxAmount = ethers.utils.parseUnits(getClaimableAmount(address).toString(), 18)
+  const proof = merkleTree.getHexProof(leafOf(address))
 
   const { pCNV, frax, dai } = getRopstenSdk(signer)
   const tokenIn = { frax, dai }[inputToken]
   const roundId = 1
-
-  const permit = await signPermit(provider, tokenIn.address, address, pCNV.address)
-
-  const a = await dai.permit(
-    formattedToAddress,
-    permit.spender,
-    permit.nonce,
-    permit.deadline,
-    true,
-    permit.v,
-    permit.r,
-    permit.s,
-  )
+  if (inputToken === 'dai') {
+    const permit = await signDaiPermit(provider, dai.address, formattedToAddress, pCNV.address)
+    const daiPermit = await dai.permit(
+      formattedToAddress,
+      permit.spender,
+      permit.nonce,
+      permit.expiry,
+      true,
+      permit.v,
+      permit.r,
+      permit.s,
+      { from: formattedToAddress },
+    )
+    daiPermit.wait(1) // do we need to wait for confirmations on this ?
+    await pCNV.claimWithPermit(
+      formattedToAddress,
+      tokenIn.address,
+      roundId,
+      maxAmount,
+      amount,
+      proof,
+      permit.expiry,
+      permit.v,
+      permit.r,
+      permit.s,
+    )
+  } else {
+    await frax.approve(formattedToAddress, maxAmount, { from: formattedToAddress })
+  }
 
   try {
     const tx = await pCNV.claimWithPermit(
