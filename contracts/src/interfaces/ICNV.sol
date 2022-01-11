05bc3c84f830e3ff07a0d66cc1c2e6d25633da3a
0xBarista <0xBarista@protonmail.com>
2022-01-11 16:09:54 -0500

    added ICNV interface, natspec cleanup

---
+++ contracts/src/interfaces/ICNV.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.8.0;
+
+interface ICNV {
+    function mint(address to, uint256 amount) external returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+}
