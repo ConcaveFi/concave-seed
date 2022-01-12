578ca0928d3a23d9aaaca9f0a1730a8c635a538c
0xBarista <0xBarista@protonmail.com>
2022-01-12 10:37:56 -0500

    test setup compiles

--- contracts/src/test/pCNV.t.sol
+++ contracts/src/test/pCNV.t.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.8.0;
 /// ============ Imports ============
 
 import { DSTest } from "ds-test/test.sol"; // DSTest
+import "./utils/VM.sol";
 // import { pCNVTest } from "./utils/pCNVTest.sol"; // Test scaffolding
 import { pCNVWhitelist } from "./utils/pCNVWhitelist.sol";
 import { IERC20 } from "@openzeppelin/token/ERC20/IERC20.sol"; // OZ: IERC20
@@ -49,5 +50,39 @@ interface IStable {
 
 
 contract pCNVTest is DSTest, pCNVWhitelist {
+
+    address immutable FRAX = 0x853d955aCEf822Db058eb8505911ED77F175b99e;
+    address immutable DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
+    address immutable treasury = 0x0877497b4A2674e818234a691bc4d2Dffcf76e73;
+
+    Vm vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+
+
+    pCNV pcnv;
+
+    function setUp() public virtual {
+        pcnv = new pCNV();
+    }
+
+
+    /* ---------------------------------------------------------------------- */
+    /*                              HELPERS                                   */
+    /* ---------------------------------------------------------------------- */
+
+    /// @notice transfer `amount` of DAI to `to`
+    function deposit_DAI(address to, uint256 amount) public {
+        address DAI_WHALE = 0xbEbc44782C7dB0a1A60Cb6fe97d0b483032FF1C7;
+        vm.startPrank(DAI_WHALE);
+        IERC20(DAI).transfer(to,amount);
+        vm.stopPrank();
+    }
+
+    /// @notice transfer `amount` of FRAX to `to`
+    function deposit_FRAX(address to, uint256 amount) public {
+        address FRAX_WHALE = 0xd632f22692FaC7611d2AA1C0D552930D43CAEd3B;
+        vm.startPrank(FRAX_WHALE);
+        IERC20(FRAX).transfer(to,amount);
+        vm.stopPrank();
+    }
 
 }
