95817dc947af23b0cb98ddd676aae9a5f809f6fd
0xBarista <0xBarista@protonmail.com>
2022-01-13 16:57:00 -0500

    IStable interface in different file and removed from tests

---
+++ contracts/src/test/utils/IStable.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity >=0.8.0;
+
+interface IStable {
+
+
+    // --- Auth ---
+  function wards() external returns ( uint256 );
+
+  function rely(address guy) external;
+
+  function deny(address guy) external;
+
+    // --- Token ---
+  function transfer(address dst, uint wad) external returns (bool);
+
+  function transferFrom(address src, address dst, uint wad) external returns (bool);
+
+  function mint(address usr, uint wad) external;
+
+  function burn(address usr, uint wad) external;
+
+  function approve(address usr, uint wad) external returns (bool);
+
+    // --- Alias ---
+  function push(address usr, uint wad) external;
+
+  function pull(address usr, uint wad) external;
+
+  function move(address src, address dst, uint wad) external;
+
+    // --- Approve by signature ---
+  function permit(address holder, address spender, uint256 nonce, uint256 expiry, bool allowed, uint8 v, bytes32 r, bytes32 s) external;
+
+
+  function balanceOf(address account) external view returns (uint256);
+}
