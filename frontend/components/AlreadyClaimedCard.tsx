1b6c471258d4ebe593e3396504e7c9d1383ce2c9
gregs <g.schrammel@icloud.com>
2022-01-13 18:10:07 -0300

    WIP PLEASE VERIFY - NEED TO UPDATE TOKEN ICON - IDK IF WORK

--- frontend/components/AlreadyClaimedCard.tsx
+++ frontend/components/AlreadyClaimedCard.tsx
@@ -1,17 +1,28 @@
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import { Button, Image, Text } from '@chakra-ui/react'
 import colors from 'theme/colors'
 import { Card } from 'components/Card'
 import { useAccount } from 'wagmi'
 import { getClaimablePCNVAmount } from 'lib/merkletree'
+import ethConfig from 'eth-sdk.config'
+import { appNetwork } from 'pages/_app'
+
+const pCNV = {
+  address: ethConfig.contracts[appNetwork.id],
+  image: '/assets/tokens/eth.svg', // tritonnn
+  symbol: 'pCNV',
+}
 
 export const AlreadyClaimedCard = () => {
   const [{ data: account }] = useAccount()
-  return (
+  return account ? (
     <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4} textAlign="center">
-      <Text>Your {getClaimablePCNVAmount(account.address)} pCNV have been claimed!</Text>
+      <Text>Your {getClaimablePCNVAmount(account.address)}pCNV have been claimed!</Text>
       <Text>Thanks for participating!</Text>
-      <Text>WAGMI</Text>
+      <Button borderRadius="xl" onClick={() => account.connector.watchAsset(pCNV)}>
+        <Image src={pCNV.image} width="24px" height="24px" mr={2} />
+        Add pCNV to wallet
+      </Button>
     </Card>
-  )
+  ) : null
 }
