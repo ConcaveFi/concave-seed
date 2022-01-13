77e5d18e0ef94802b203365e615aa5f440cad197
NUnicorn2860 <unicornewborn@gmail.com>
2022-01-12 17:53:42 -0800

    Path rewrites

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: '/about',
+        destination: '/',
+      },
+    ]
+  },
   reactStrictMode: true,
 };
