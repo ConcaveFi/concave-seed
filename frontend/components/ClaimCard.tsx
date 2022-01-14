9ec2f6e65d17c965ecead85e8ee1d202b8333a6b
gregs <g.schrammel@icloud.com>
2022-01-14 01:36:33 -0300

    pingus on the discord

--- frontend/components/ClaimCard.tsx
+++ frontend/components/ClaimCard.tsx
@@ -1,5 +1,5 @@
 import React, { useCallback, useEffect, useState } from 'react'
-import { Button } from '@chakra-ui/react'
+import { Button, Link, Stack, Text } from '@chakra-ui/react'
 import { Card } from 'components/Card'
 import colors from 'theme/colors'
 import { AmountInput } from './Input'
@@ -33,26 +33,35 @@ export function ClaimCard({ signer, afterSuccessfulClaim }) {
   }
 
   return (
-    <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4}>
-      <AmountInput
-        maxAmount={claimableAmount}
-        value={amount}
-        onChangeValue={setAmount}
-        tokenOptions={inputTokens}
-        selectedToken={inputToken}
-        onSelectToken={setInputToken}
-      />
-      <Button
-        onClick={onClaim}
-        isLoading={!signer || isLoading}
-        isDisabled={!signer || Number(amount) < 1}
-        variant="primary"
-        size="large"
-        fontSize={24}
-        isFullWidth
-      >
-        Claim pCNV
-      </Button>
-    </Card>
+    <Stack spacing={3} align="center">
+      <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4}>
+        <AmountInput
+          maxAmount={claimableAmount}
+          value={amount}
+          onChangeValue={setAmount}
+          tokenOptions={inputTokens}
+          selectedToken={inputToken}
+          onSelectToken={setInputToken}
+        />
+        <Button
+          onClick={onClaim}
+          isLoading={!signer || isLoading}
+          isDisabled={!signer || Number(amount) < 1}
+          variant="primary"
+          size="large"
+          fontSize={24}
+          isFullWidth
+        >
+          Claim pCNV
+        </Button>
+      </Card>
+      <Text fontSize="sm" color="text.3" maxW={400} textAlign="center">
+        Feel free to ping us in{' '}
+        <Link color="text.highlight" href="https://discord.gg/tB3tPby3">
+          discord
+        </Link>{' '}
+        if you need help claiming using a multisig
+      </Text>
+    </Stack>
   )
 }
