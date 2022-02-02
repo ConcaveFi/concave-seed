8bc39c9d47394b6b77293a671ebf3c2348e06317
gregs <g.schrammel@icloud.com>
2022-02-02 01:50:59 -0300

    fix some warnings

--- frontend/components/TopBar/ConnectWallet.tsx
+++ frontend/components/TopBar/ConnectWallet.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Button, Image, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
 import { useAccount, useConnect } from 'wagmi'
 import colors from 'theme/colors'
+import { useIsMounted } from 'hooks/useIsMounted'
 
 const miniAddress = (address) =>
   `${address.substr(0, 6)}...${address.substr(address.length - 6, address.length)}`
@@ -24,6 +25,7 @@ const DisconnectButton = () => {
 
 const ConnectButton = ({ onError }: { onError: (e: Error) => void }) => {
   const [{ data, error }, connect] = useConnect()
+  const isMounted = useIsMounted()
   return (
     <>
       <Menu placement="bottom-end" isLazy>
@@ -37,20 +39,21 @@ const ConnectButton = ({ onError }: { onError: (e: Error) => void }) => {
           Connect wallet
         </MenuButton>
         <MenuList bg="green.500" borderRadius="xl" minW="min" px={1}>
-          {data.connectors.map((connector) => {
-            if (!connector.ready) return null
-            // change image from using connector id to something else, injected can be metamask, coinbase, brave etc
-            return (
-              <MenuItem
-                borderRadius="xl"
-                icon={<Image maxWidth="20px" src={`/connectors/${connector.id}.png`} alt="" />}
-                key={connector.id}
-                onClick={() => connect(connector)}
-              >
-                {connector.name}
-              </MenuItem>
-            )
-          })}
+          {isMounted &&
+            data.connectors.map((connector) => {
+              if (!connector.ready) return null
+              // change image from using connector id to something else, injected can be metamask, coinbase, brave etc
+              return (
+                <MenuItem
+                  borderRadius="xl"
+                  icon={<Image maxWidth="20px" src={`/connectors/${connector.id}.png`} alt="" />}
+                  key={connector.id}
+                  onClick={() => connect(connector)}
+                >
+                  {connector.name}
+                </MenuItem>
+              )
+            })}
         </MenuList>
         {/* <UnsuportedNetworkModal
           isOpen={state === 'unsupportedNetwork'}
