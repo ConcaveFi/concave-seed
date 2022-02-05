01a19c743ef23ecfefa051350e674c6c1c579c79
ead139 <erickdelgado@protonmail.com>
2022-02-05 09:53:26 -0500

    wave 4

--- frontend/components/NotWhitelistedCard.tsx
+++ frontend/components/NotWhitelistedCard.tsx
@@ -6,8 +6,7 @@ import {Heading, Link, Text } from '@chakra-ui/react'
 export const NotWhitelistedCard = () => {
   return (
     <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4}>
-     <Heading fontSize="xx-large" color="" textAlign="center">This wallet is not whitelisted for Wave 3!</Heading>    
-      <Text textAlign="center">Wave 4: Feb 5, 2022 at 3PM UTC / 10AM EST </Text>
+     <Heading fontSize="xx-large" color="" textAlign="center">This wallet is not whitelisted for Wave 4!</Heading>    
       <Text textAlign="center">The whitelist sale will be launched on ETH mainnet in waves, we want to reward our most loyal supporters and also make it an enjoyable experience without a typical rush of people crashing a website!</Text> 
       <Text textAlign="center">Please ensure you know which wave category you are in and that you connect with your whitelisted address.</Text>
       <Text textAlign="center">*In order to participate in this token sale, please ensure that you have enough FRAX or DAI in your wallet to make your purchase. Please also ensure that you have some $ETH to cover the gas fees when interacting with the contract.</Text>
