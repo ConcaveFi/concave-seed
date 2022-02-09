3f6533fe9869144307ebadd4d295c9209f830d04
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-02-09 16:59:30 -0500

    aCNV concluded

--- frontend/components/NotWhitelistedCard.tsx
+++ frontend/components/NotWhitelistedCard.tsx
@@ -6,11 +6,8 @@ import {Heading, Link, Text } from '@chakra-ui/react'
 export const NotWhitelistedCard = () => {
   return (
     <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4}>
-     <Heading fontSize="xx-large" color="" textAlign="center">This wallet is not whitelisted for Wave 4!</Heading>    
-      <Text textAlign="center">The whitelist sale will be launched on ETH mainnet in waves, we want to reward our most loyal supporters and also make it an enjoyable experience without a typical rush of people crashing a website!</Text> 
-      <Text textAlign="center">Please ensure you know which wave category you are in and that you connect with your whitelisted address.</Text>
-      <Text textAlign="center">*In order to participate in this token sale, please ensure that you have enough FRAX or DAI in your wallet to make your purchase. Please also ensure that you have some $ETH to cover the gas fees when interacting with the contract.</Text>
-      <Text color="text.3" textAlign="center"> Find out more information about Concave and the whitelist {' '}
+     <Heading fontSize="xx-large" color="" textAlign="center">Presale for aCNV has concluded!</Heading>    
+      <Text color="text.3" textAlign="center"> Find more information about Concave and the presale {' '}
         <Link color="text.highlight" href="https://concave.lol/blog/concave-whitelist?utm_source=seed_app&utm_medium=Website&utm_campaign=concave-whitelist&utm_content=no_whitelist_redirect">
           here
         </Link>
