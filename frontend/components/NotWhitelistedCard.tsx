5bb5d7facd6df02d9bd9a7b24f7ac1f638a111c4
shanghigh <95733088+shanghigh@users.noreply.github.com>
2022-02-09 17:40:07 -0500

    Update NotWhitelistedCard.tsx
    hyperlink update

--- frontend/components/NotWhitelistedCard.tsx
+++ frontend/components/NotWhitelistedCard.tsx
@@ -8,7 +8,7 @@ export const NotWhitelistedCard = () => {
     <Card shadow="up" bgGradient={colors.gradients.green} px={10} py={8} gap={4}>
      <Heading fontSize="xx-large" color="" textAlign="center">Presale for aCNV has concluded!</Heading>    
       <Text color="text.3" textAlign="center"> Find more information about Concave and the presale {' '}
-        <Link color="text.highlight" href="https://concave.lol/blog/concave-whitelist?utm_source=seed_app&utm_medium=Website&utm_campaign=concave-whitelist&utm_content=no_whitelist_redirect">
+        <Link color="text.highlight" href="https://concave.lol/blog/">
           here
         </Link>
       </Text>
